55ce8ad8b07c5c437659754b811a5e44d35fd53e
mtfr1 <mateusfigueiredo@dcc.ufmg.br>
2021-11-23 19:50:34 -0300

    [frontend] fetching category from API and some design adjustments

--- frontend/src/views/post/postByCategory/postByCategory.vue
+++ frontend/src/views/post/postByCategory/postByCategory.vue
@@ -1,17 +1,19 @@
 <template>
     
-    <div id="PostByCategory" style="background-color:#CCC8B3; width:250px; margin-left:15%; margin-right:15%">
-        <select @change="setCategoria($event)" class="form-select form-control">
+    <div id="PostByCategory" style="background-color:#CCC8B3; margin-left:15%; margin-right:15%">
+        <select @change="setCategoria($event)" class="form-select form-control" style="width:250px;  margin-left:10%;">
             <option> --- Selecione uma categoria --- </option>
-            <option v-for="categoria in categorias" :key="categoria.id" :value="categoria"> {{ categoria.nome }} </option>
+            <option v-for="categoria in this.categorias" :key="categoria.id" :value="categoria.id"> {{ categoria.nome }} </option>
         </select>
+        <br/>
         <div v-if="categoriaSelecionada != null">
-            <div v-for="postagem in posts" :key="postagem.id"  style="background-color:#CCC8B3; margin-left:15%; margin-right:15%">
+            <div v-for="postagem in this.posts" :key="postagem.id"  style="background-color:#CCC8B3; margin-left:15%; margin-right:15%">
                 <h3>Título: {{ postagem.titulo }}</h3>
                 <small>Categorias: {{ postagem.categorias }}</small>
                 <small>Autor:{{ postagem.criador }}</small>
                 <br/>
                 <p>{{ postagem.conteudo }}</p>
+                <br/>
                 <button class="btn btn-info" style="margin-left:20px" >Seguir Post</button>
             </div>
         </div>
@@ -28,7 +30,7 @@
             categoriaSelecionada: null,
             categorias: {},
             post:{}, 
-            posts: []
+            posts: {}
             }
         },
         
@@ -43,12 +45,31 @@
                 var response = await fetch('http://127.0.0.1:8000/api/categoria');
                 this.categorias = await response.json();
             },
-
             setCategoria(e)
             {
+                this.getPostagens(e.target.value)
                 this.categoriaSelecionada = e.target.value ;
-                console.log("akajehae" + e.target.value.id );
-            }
+            },
+            async getPostagens(selecionado)
+            {
+                var response = await fetch(`http://127.0.0.1:8000/api/categoria/${selecionado}`);
+                var categoria = await response.json();
+                console.log("posts.size(): "+ categoria.posts.length);
+                var arrayLength = categoria.posts.length;
+                this.posts = [];
+
+                for (let i = 0; i < arrayLength; i++) {
+                    console.log("element: " +categoria.posts[i]);
+                    var response2 = await fetch(`http://127.0.0.1:8000/api/post/${categoria.posts[i]}`);
+                    var temp_post = await response2.json();
+                    console.log("post: "+ temp_post);
+                    console.log("post title: " +temp_post.titulo)
+                    this.posts += temp_post;
+                }               
+                
+            },
+            
+
             
         }
 
